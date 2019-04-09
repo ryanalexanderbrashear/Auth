c924008ae632386b8e73cc912909a0ad2a6bee4c
Alex Brashear <alexbrashear@gmail.com>
2019-04-09 14:25:29 -0400

    Added in Firebase. Also, did some UI work for the authentication card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Button, Card, CardSection, Header } from './components/common';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
+import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyC2TsVgXsgCB0WPo3b8oKCM4nKYDbuKNLc",
+            authDomain: "auth-216ad.firebaseapp.com",
+            databaseURL: "https://auth-216ad.firebaseio.com",
+            projectId: "auth-216ad",
+            storageBucket: "auth-216ad.appspot.com",
+            messagingSenderId: "360708838472"
+        });
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An App</Text>
+                <LoginForm />
             </View>
         );
     }
