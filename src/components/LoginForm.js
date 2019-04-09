d91d1c8e85e5f2755c7e37469e760df478ee0a2f
Alex Brashear <alexbrashear@gmail.com>
2019-04-09 16:01:44 -0400

    Login functionality is completed, along with an error message for fails

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,26 +1,71 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common'; 
 
 class LoginForm extends Component {
-    state = { text: '' };
+    state = { 
+        email: '',
+        password: '',
+        error: ''
+    };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(() => {
+            firebase.auth().createUserWithEmailAndPassword(email, password).catch(() => {
+                this.setState({ error: 'Authentication failed.' });
+            });
+        });
+    }
 
     render() {
+        const { errorTextStyle } = styles;
+
         return (
             <Card>
                 <CardSection>
-                    <Input label='Email' value={this.state.text} onChangeText={text => this.setState({ text: text })} />
+                    <Input 
+                        label='Email' 
+                        placeholder='user@gmail.com' 
+                        value={this.state.email} 
+                        onChangeText={email => this.setState({ email })} 
+                    />
                 </CardSection>
                 
                 <CardSection>
-
+                    <Input 
+                        label='Password' 
+                        placeholder='password'
+                        secureTextEntry
+                        value={this.state.password} 
+                        onChangeText={password => this.setState({ password })} 
+                    />
                 </CardSection>
 
+                <Text style={errorTextStyle}>
+                    {this.state.error} 
+                </Text>
+
                 <CardSection>
-                    <Button title="Login" onPress={() => console.log('Pressed')} />
+                    <Button 
+                        title="Login" 
+                        onPress={this.onButtonPress.bind(this)} 
+                    />
                 </CardSection>
             </Card>
         );
     }
 }
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red',
+        marginTop: 10,
+        marginBottom: 10
+    }
+};
 
 export default LoginForm;
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
 
     const { inputStyle, labelStyle, containerStyle } = styles;
 
     return (
         <View style={ containerStyle }>
             <Text style={ labelStyle }>{ label }</Text>
-            <TextInput value={ value } onChangeText={ onChangeText } style={ inputStyle } />
+            <TextInput
+                autoCapitalize={ 'none' }  
+                secureTextEntry={ secureTextEntry } 
+                placeholder={ placeholder } 
+                autoCorrect={ false } 
+                value={ value } 
+                onChangeText={ onChangeText } 
+                style={ inputStyle } />
         </View>
     );
 };
