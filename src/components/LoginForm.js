9cba9a44abc45e20325e0ee03e14eaf57092680e
Alex Brashear <alexbrashear@gmail.com>
2019-04-09 14:53:02 -0400

    Working with text inputs

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common'; 
+import { Button, Card, CardSection, Input } from './common'; 
 
 class LoginForm extends Component {
+    state = { text: '' };
+
     render() {
         return (
             <Card>
                 <CardSection>
-                    <TextInput style={{ height: 20, width: 100 }} />
+                    <Input label='Email' value={this.state.text} onChangeText={text => this.setState({ text: text })} />
                 </CardSection>
                 
                 <CardSection>
